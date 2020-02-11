126c424b37f06c61f22c491b95694a8592baa6b1
mario vargas <jorgemario2pac@utp.edu.co>
2020-02-11 16:38:30 -0500

    termino buscardor

--- 02-spa/src/app/components/busqueda/busqueda.component.ts
+++ 02-spa/src/app/components/busqueda/busqueda.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Heroe, HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute } from '@angular/router';
 import { log } from 'util';
+import { HeroesComponent } from './../heroes/heroes.component';
+
 
 
 @Component({
@@ -12,16 +14,16 @@ import { log } from 'util';
 export class BusquedaComponent implements OnInit {
 
   heroes: any[] = [];
-  termino: string
+  termino: string;
 
   // tslint:disable-next-line: no-trailing-whitespace
-  constructor(private activatedRoute: ActivatedRoute, private _heroesService: HeroesService ) { 
+  constructor(private activatedRoute: ActivatedRoute, private _heroesService: HeroesService ) {
 
   }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe( params => {
-      console.log(params['termino']);
+      this.termino = params['termino'];
       this.heroes = this._heroesService.buscarHeroes( params['termino'] );
       console.log(this.heroes);
     });
